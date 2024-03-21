5a9c30e461ad3232888a6f1bc122565668564fa2
Nat-crit20 <greuelnathan74@gmail.com>
2024-03-21 17:48:56 -0500

    Created the Renderer class and the constructor

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,1 +1,16 @@
+class Renderer {
+  constructor(scale) {
+    //Chip-8 is 64x32 pixels.
+    this.cols = 64;
+    this.rows = 32;
+    this.scale = scale;
 
+    this.canvas = document.querySelector("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    //Scale up the display to make it user friendly
+    this.canvas.width = this.cols * scale;
+    this.canvas.height = this.rows * scale;
+    this.display = new Array(this.cols * this.rows);
+  }
+}
+export default Renderer;
